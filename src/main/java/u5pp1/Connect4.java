495d7cde238b10ed0ff54f35acc02382307ac393
Owen Swanson <owsw333@vcs.email>
2023-01-11 21:33:55 -0800

    this was the hardest project i have ever done, I need help with traversing 2d arrays because this project has shown me that it is something i clearly do not understand

--- src/main/java/u5pp1/Connect4.java
+++ src/main/java/u5pp1/Connect4.java
@@ -12,7 +12,75 @@ public class Connect4 {
     public static final int EMPTY = 0;
 
    // implementation here
+    /*returns true if all the board spaces have a piece in them. 
+    Red pieces are indicated by `1` and black pieces are indicated by `-1`. Empty spaces are indicated by `0`.*/
+    public static boolean isFull(int[][] board){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                if (board[i][j] == 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+    //returns true if the board is a valid configuration. A board is valid if there are no floating pieces.
+    public static boolean isBoardValid(int[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if( board [i][j] == 1 || board [i][j] == -1){
+                    for(int k = i+1; k < board.length; k++){
+                        if (board[k][j] == 0){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /*returns `RED_WIN` if only red wins, and `BLACK_WIN` if only black wins. Returns `NO_WINNER` if neither have won. 
+    Returns `BOTH_WIN` if both red and black have won. Each of these return variables are found at the top of the class.*/
+    public static int getWinner(int[][] board){
+        if (isVertical(board, BLACK) == true){
+            return BLACK_WIN;
+        }else if (isVertical(board, RED) == true){
+            return RED_WIN;
+        } else if(isHorizontal(board, BLACK) == true){
+            return BLACK_WIN;
+        } else if(isHorizontal(board, RED) == true){
+            return RED_WIN;
+        }else{
+            return NO_WINNER;
+        }
+            
+        }
+    
    // It is recommended to use private helper methods
 
-}
+    //horizontal works
+    private static boolean isHorizontal(int[][]bord, int num){
+        for(int i = 0; i < bord.length; i++){
+            for (int j = 0; j < bord[i].length-3; i++){
+                if(bord[i][j] == num){
+                    if (bord[i][j+1] == num && bord[i][j+2] == num && bord[i][j+3] == num){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isVertical(int[][] bord,int num){
+        for(int i = 0; i < bord.length; i++){
+            for (int j = 0; j < bord[i].length; i++){
+                
+            }
+        }
+        return false;
+    }
+    }
+
